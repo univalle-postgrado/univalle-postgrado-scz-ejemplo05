9f6a3129b6dd9ab821f8377d0fe75fd9b9bc4cf7
Marcelo Quispe Ortega <marceloquispeortega@gmail.com>
2025-02-18 18:19:46 -0400

    Importación del módulo de TypeORM al módulo de movies

--- src/movies/movies.module.ts
+++ src/movies/movies.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MoviesService } from './movies.service';
 import { MoviesController } from './movies.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Movie } from './entities/movie.entity';
+import { Category } from 'src/categories/entities/category.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Movie, Category])],
   controllers: [MoviesController],
   providers: [MoviesService],
 })
