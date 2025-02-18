6aa33bfb58d89a8f98044cfa1440aa024c689d5d
Marcelo Quispe Ortega <marceloquispeortega@gmail.com>
2025-02-18 17:08:22 -0400

    Configuración de la aplicación para que utilice Pipes para realizar la validación

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(new ValidationPipe({
+    stopAtFirstError: true,
+  }));
+
+  const configService = app.get(ConfigService);
+  await app.listen(configService.get<number>('PORT') || 3000);
 }
 bootstrap();
