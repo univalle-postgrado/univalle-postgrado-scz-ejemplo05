bf4360fff6b2509ee0c03743d4e5edc033506763
Marcelo Quispe Ortega <marceloquispeortega@gmail.com>
2025-02-18 17:10:34 -0400

    COnfiguración de la entidad para que esta pueda mapearse a la base de datos

--- src/categories/entities/category.entity.ts
+++ src/categories/entities/category.entity.ts
@@ -1,1 +1,25 @@
-export class Category {}
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, Unique, UpdateDateColumn } from "typeorm";
+
+@Entity('categories')
+@Unique(['title'])
+export class Category {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ length: 30 })
+    title: string;
+
+    @Column({ length: 180, nullable: true })
+    description: string;
+
+    @Column({ default: false })
+    enabled: boolean;
+
+    @CreateDateColumn({ type: 'timestamp without time zone' })
+    created_at: Date;
+
+    @UpdateDateColumn({ type: 'timestamp without time zone', nullable: true })
+    updated_at: Date;
+
+}
